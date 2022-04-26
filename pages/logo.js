dc6d3df70a6e6236eb74816269233783817c9788
Ryan Taylor <59121627+ryant74@users.noreply.github.com>
2022-04-26 02:44:38 -0700

    navbar changes & scroll animation

--- pages/logo.js
+++ pages/logo.js
@@ -2,8 +2,18 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 
 const Logo = () => {
+
+    //this is horrible react practice, but it looks very nice
+    const animScroll = () => {
+        let id = setInterval(function() {
+            document.scrollingElement.scrollTop *= 0.91;
+            if(document.scrollingElement.scrollTop <= 0) { clearInterval(id); }
+        }, 10);
+    }
+
+
     return (
-        <div class="logo">
+        <div class="logo" onClick={() => animScroll()}>
             <div class="image"/>
         </div>
     )
