36b5ca8eac0e2a0d1dba2d1cca28002d6ec12d45
Ryan Taylor <59121627+ryant74@users.noreply.github.com>
2022-06-01 16:25:44 -0700

    Commit keys for grading

---
+++ components/config.js
@@ -0,0 +1,11 @@
+export function DBCONF() {
+    return {
+        "apiKey": "AIzaSyBRbRxBtxbIC6o5GOzuIRPTICUQEF-pU7Q",
+        "authDomain": "nexus-9d26f.firebaseapp.com",
+        "projectId": "nexus-9d26f",
+        "storageBucket": "nexus-9d26f.appspot.com",
+        "messagingSenderId": "965590920902",
+        "appId": "1:965590920902:web:e33aefdd23115019cf71c7",
+        "measurementId": "G-H483K1SNC1"
+    };
+}
